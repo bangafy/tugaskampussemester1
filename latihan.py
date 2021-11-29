bbf57ba61ecdb62270bb526dc0278ae8636371f7
bangafy <94420401+bangafy@users.noreply.github.com>
2021-11-29 15:39:42 +0700

    latihan

---
+++ latihan.py
@@ -0,0 +1,39 @@
+# contoh 1 list
+# my_list = ["1", "love", "you","progamming"]
+# print(my_list[1])
+# print(my_list[3])
+
+# contoh 2 list bersarangs
+# your_list = ["hello", [1, 9, 9, 3], "afy"]
+# print(your_list[1][2])
+# indexs error (melebihi total index [your_listnya])
+
+# contoh 3 list negatif
+# my_list2 = ['b', 'a', 'n', 'g', 'a', 'f', 'y']
+# print(my_list2[-3])
+# jawaban = a (di hitung dari sebelah kiri)
+
+# contoh 4slicing list
+# my_list2 = ['b', 'a', 'n', 'g', 'a', 'f', 'y']
+# print(my_list2[3:6])
+# print(my_list2[0:])
+# print(my_list2[:7])
+# print(my_list2[-3:-6])
+
+# contoh 5 ubah anggota l
+# ganjil = [1, 2, 3, 4]
+# print(ganjil)
+
+# ganjil[2] = 4
+# print(ganjil)
+
+# mengubah sekaligus
+# ganjil[2:4] = [11, 12, 13]
+# print(ganjil)
+
+# contoh 6 menambah anggota list
+# ganjil = [1, 3, 5, 7]
+# ganjil.append(9)
+# print(ganjil)
+# ganjil.extend([11, 13])
+# print(ganjil)
